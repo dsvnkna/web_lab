45ba0d5879fa783eea03a9a83ae826ed9b6fed82
dsvnkna <dsavinkina@gmail.com>
2018-01-15 10:14:33 +0200

    lab9

--- bootstrap/js/news.js
+++ bootstrap/js/news.js
@@ -1,3 +1,9 @@
+var useLocalStorage = false;
+
+function switchUseLS(){
+	useLocalStorage = !useLocalStorage;
+}
+
 function isOnline() {
 	return window.navigator.onLine;
 }
@@ -57,10 +63,40 @@ function addNews(){
 }
 
 function addToStorage(newsItem){
-	var news = getNews();
-    news.push(newsItem);
-    localStorage.setItem('news', JSON.stringify(news));
-    return false;
+	if(useLocalStorage){
+		var news = getNews();
+	    news.push(newsItem);
+	    localStorage.setItem('news', JSON.stringify(news));
+	    return false;
+	} else{
+		var openDB = indexedDB.open("news", 1);
+
+		openDB.onerror = function(event) {
+		  alert("Error occurred when loading news");
+		};
+		openDB.onupgradeneeded = function() {
+		    var db = openDB.result;
+		    var store = db.createObjectStore("news", {keyPath: "header"});
+		    store.createIndex("header", "header", { unique: false });
+		    store.createIndex("shortText", "shortText", { unique: false });
+		    store.createIndex("fullText", "fullText", { unique: false });
+		    store.createIndex("image", "image", { unique: false });
+		};
+		openDB.onsuccess = function(event) {
+			var db = openDB.result;
+			var tx = db.transaction(["news"], "readwrite");
+			var store = tx.objectStore("news");
+			var addFeedback = store.put(newsItem);
+			addFeedback.onsuccess = function(event){
+			}
+			addFeedback.onerror = function(event){
+				alert("Error occurred when loading news");
+			}
+			tx.oncomplete = function(){
+				db.close();
+			}
+		};
+	}
 }
 
 function loadPreviewPhoto(){
@@ -80,13 +116,44 @@ function createNews(news){
 }
 
 function show(){
-	if(isOnline()){
-		//server stuff
-	}
-	var news = getNews();
-    if ((typeof news !== 'undefined') && (news.length > 0)) {
-	    for(var i = 0; i < news.length; i++) {
-    		createNews(news[i]);
+	if(useLocalStorage){
+		var news = new Array;
+	    var news_item = localStorage.getItem('news');
+	    if (news_item !== null) {
+	        news = JSON.parse(news_item);
 	    }
+	    if ((typeof news !== 'undefined') && (news.length > 0)) {
+		    for(var i = 0; i < news.length; i++) {
+	    		createNews(news[i]);
+		    }
+		}
+	} else{
+		var openDB = indexedDB.open("news", 1);
+		openDB.onupgradeneeded = function() {
+		    var db = openDB.result;
+		    var store = db.createObjectStore("news", {keyPath: "header"});
+		    store.createIndex("header", "header", { unique: false });
+		    store.createIndex("shortText", "shortText", { unique: false });
+		    store.createIndex("fullText", "fullText", { unique: false });
+		    store.createIndex("image", "image", { unique: false });
+		}
+		openDB.onsuccess = function(event) {
+			var db = openDB.result;
+			var tx = db.transaction("news", "readwrite");
+	    	var store = tx.objectStore("news");
+	    	store.openCursor().onsuccess = function(event) {
+				var cursor = event.target.result;
+				if (cursor) {
+					var tempNews = new News(cursor.value.header, cursor.value.shortText, cursor.value.fullText, cursor.value.image);
+					//console.log(tempFeed);
+				  	//feedbacks.push(tempFeed);
+				  	createNews(tempNews);
+				  	cursor.continue();
+				}
+			};
+	    	tx.oncomplete = function(){
+	    		db.close();
+	    	}
+		}
 	}
 }
